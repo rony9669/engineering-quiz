0267e73fbed8f8bfb66d13eb300e15ff74aae658
rony9669 <ronychowdhury.cse@gmail.com>
2022-10-11 13:32:40 +0600

    question option  added

--- src/Components/QuizDetails/QuizDetails.js
+++ src/Components/QuizDetails/QuizDetails.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import QuizQuestion from "../QuizQuestion/QuizQuestion";
 
 const QuizDetails = () => {
   const quiz = useLoaderData();
   const { name } = quiz.data;
-  console.log(quiz);
-  console.log(quiz.data.questions);
+  // console.log(quiz);
+  // console.log(quiz.data.questions);
   return (
     <div>
       <h1>
         The exam is about: <span>{name}</span>
       </h1>
+      {quiz.data.questions.map((ques) => (
+        <QuizQuestion key={ques.id} ques={ques}></QuizQuestion>
+      ))}
     </div>
   );
 };
--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -1,9 +1,42 @@
 import React from "react";
 
-const QuizQuestion = () => {
+const QuizQuestion = ({ ques }) => {
+  console.log(ques.options[0]);
+  const { question } = ques;
+  //   console.log(question);
+
+  const newQuestion = question.replace(/(<([^>]+)>)/gi, "");
+  //   console.log(newStr);
+
   return (
     <div>
-      <h1> This is form quiz question </h1>
+      <ul class="quiz">
+        <li>
+          <h4>How many letters are there in "JS"?</h4>
+          <ul class="choices">
+            <li>
+              {" "}
+              <input type="radio" name="question0" value="A" />
+              <span>2</span>{" "}
+            </li>
+            <li>
+              {" "}
+              <input type="radio" name="question0" value="B" />
+              <span>1</span>{" "}
+            </li>
+            <li>
+              {" "}
+              <input type="radio" name="question0" value="C" />
+              <span>3</span>{" "}
+            </li>
+            <li>
+              {" "}
+              <input type="radio" name="question0" value="D" />
+              <span>4</span>{" "}
+            </li>
+          </ul>
+        </li>
+      </ul>
     </div>
   );
 };
