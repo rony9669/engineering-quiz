c66ff5fc021ee98badb6a77033ed3d2fd09ce05b
rony9669 <ronychowdhury.cse@gmail.com>
2022-10-11 20:00:46 +0600

    right answer toast added

--- src/Components/QuizQuestion/QuizQuestion.js
+++ src/Components/QuizQuestion/QuizQuestion.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faEye } from "@fortawesome/free-solid-svg-icons";
 import QuestionOption from "../QuestionOption/QuestionOption";
 
 const QuizQuestion = ({ ques }) => {
-  // console.log(ques.correctAnswer);
-  const { question, correctAnswer } = ques;
+  // console.log(ques);
+  const { question, correctAnswer, id } = ques;
   //   console.log(question);
 
   const newQuestion = question.replace(/(<([^>]+)>)/gi, "");
@@ -14,17 +16,25 @@ const QuizQuestion = ({ ques }) => {
     const theText = event.currentTarget.textContent;
     console.log("the text: ", theText);
     if (theText === correctAnswer) {
-      console.log("the answer is true");
       toast.success("Your Answer is Correct", { autoClose: 1000 });
     } else {
-      console.log("the answer is false");
       toast.warning("Your Answer is Wrong", { autoClose: 1000 });
     }
   };
+  const showAnswer = (answer) => {
+    console.log(answer);
+    toast.success(answer, { autoClose: 1000 });
+  };
 
   return (
-    <div>
-      <div className="bg-emerald-200 my-10 p-5 mx-5">
+    <div className="bg-emerald-200 my-10 p-5 mx-5  ">
+      <div
+        className=" text-end cursor-pointer"
+        onClick={() => showAnswer(correctAnswer)}
+      >
+        <FontAwesomeIcon icon={faEye} />
+      </div>
+      <div>
         <div>
           <h1 className="text-center font-semibold">Question:{newQuestion}</h1>
         </div>
